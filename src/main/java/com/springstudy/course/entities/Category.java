db8517480a864298e469aaac63a65a94b36a11c3
charlles silva <magrohue10@gmail.com>
2022-04-19 20:18:44 -0300

    product

--- src/main/java/com/springstudy/course/entities/Category.java
+++ src/main/java/com/springstudy/course/entities/Category.java
@@ -2,7 +2,10 @@ package com.springstudy.course.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -13,6 +16,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category(){
     }
@@ -36,6 +42,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
